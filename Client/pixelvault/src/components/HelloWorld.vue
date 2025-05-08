283bf140a799401c6e379a88679085c1407f56b3
Adrián <145448579+aadrii09@users.noreply.github.com>
2025-05-08 09:23:11 +0200

    Initial project setup: Vue with Tailwind CSS configuration

---
+++ Client/pixelvault/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 flex iteams-center justify-content">
+    <h1 class="taxt-4x1 font bold text-indigo-600">Hello world</h1>
+  </div>
+</template>
