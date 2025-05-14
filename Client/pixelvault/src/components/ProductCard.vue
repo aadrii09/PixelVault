0437d70bcaedac1cae63adc4d42777ac4bd2762c
Adrián <145448579+aadrii09@users.noreply.github.com>
2025-05-14 14:07:36 +0200

    bug description fixed

--- Client/pixelvault/src/components/ProductCard.vue
+++ Client/pixelvault/src/components/ProductCard.vue
@@ -23,7 +23,7 @@ import { agregarAlCarrito } from '../api/carrito';
     <div class="bg-white p-4 rounded shadow hover:shadow-lg transition">
         <img :src="props.producto.imagenUrl || 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'" alt="Product Image" class="w-full h-40 object-cover rounded mb-3">
         <h3 class="text-lg font-semibold">{{ props.producto.nombre }}</h3>
-        <p class="text-sm text-gray-500">{{ props.producto.description?.slice(0, 50) }}...</p>
+        <p class="text-sm text-gray-500">{{ props.producto.descripcion?.slice(0, 50) }}...</p>
         <p class="mt-2 font-bold text-blue-600">{{ props.producto.stock }}</p>
         <button
             v-if="props.mostrarBoton"
